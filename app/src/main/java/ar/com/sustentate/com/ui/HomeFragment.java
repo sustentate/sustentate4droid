067c556209b6cf87a66a1e5c62b31b97e6b3ac7f
Pablo Cavallo <pac@codebox.com.ar>
2018-04-19 09:00:59 -0700

    Se cambio el nombre del paquete

--- app/src/main/java/ar/com/sustentate/com/ui/HomeFragment.java
+++ app/src/main/java/ar/com/sustentate/com/ui/HomeFragment.java
@@ -5,8 +5,6 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
-import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
