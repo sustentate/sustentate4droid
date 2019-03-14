0b34f6e6d4aef33c57b9d9bc6b48852c4ec77eb8
Sebastian Zumbay <emzash.1@gmail.com>
2019-03-14 00:03:20 -0300

    button hoe

--- app/src/main/java/ar/com/sustentate/com/ui/HomeFragment.java
+++ app/src/main/java/ar/com/sustentate/com/ui/HomeFragment.java
@@ -4,18 +4,14 @@ package ar.com.sustentate.com.ui;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
-
-import com.bumptech.glide.Glide;
-import com.bumptech.glide.request.RequestOptions;
 
 import ar.com.sustentate.com.R;
 import ar.com.sustentate.com.utils.KeySaver;
-import jp.wasabeef.glide.transformations.BlurTransformation;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -47,6 +43,15 @@ public class HomeFragment extends Fragment {
             @Override
             public void onClick(View v) {
                         KeySaver.removeKey((Activity) fragmentView.getContext(), "sliding");
+                startActivity(new Intent(fragmentView.getContext(), MainActivity.class));
+            }
+        });
+
+        FloatingActionButton how = (FloatingActionButton) fragmentView.findViewById(R.id.how);
+        how.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                KeySaver.removeKey((Activity) fragmentView.getContext(), "sliding");
                 startActivity(new Intent(fragmentView.getContext(), SlideActivity.class));
             }
         });
